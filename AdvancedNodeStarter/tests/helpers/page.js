b0cb74559b3b560a3c058df23163f740ec32fbe9
muhammadzain8 <muhammadzain8@gmail.com>
2022-03-07 11:58:59 +0500

    few changes

--- AdvancedNodeStarter/tests/helpers/page.js
+++ AdvancedNodeStarter/tests/helpers/page.js
@@ -6,16 +6,18 @@ class CustomPage {
   static async build() {
     const browser = await puppeteer.launch({
       headless: true,
-      args: ['--no-sandbox']
+      args: ['--no-sandbox'],
     });
 
     const page = await browser.newPage();
     const customPage = new CustomPage(page);
 
     return new Proxy(customPage, {
-      get: function(target, property) {
-        return customPage[property] || browser[property] || page[property];
-      }
+      get: function (target, property) {
+        return (
+          customPage[property] || browser[property] || page[property]
+        );
+      },
     });
   }
 
@@ -34,18 +36,18 @@ class CustomPage {
   }
 
   async getContentsOf(selector) {
-    return this.page.$eval(selector, el => el.innerHTML);
+    return this.page.$eval(selector, (el) => el.innerHTML);
   }
 
   get(path) {
-    return this.page.evaluate(_path => {
+    return this.page.evaluate((_path) => {
       return fetch(_path, {
         method: 'GET',
         credentials: 'same-origin',
         headers: {
-          'Content-Type': 'application/json'
-        }
-      }).then(res => res.json());
+          'Content-Type': 'application/json',
+        },
+      }).then((res) => res.json());
     }, path);
   }
 
@@ -56,10 +58,10 @@ class CustomPage {
           method: 'POST',
           credentials: 'same-origin',
           headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
           },
-          body: JSON.stringify(_data)
-        }).then(res => res.json());
+          body: JSON.stringify(_data),
+        }).then((res) => res.json());
       },
       path,
       data
